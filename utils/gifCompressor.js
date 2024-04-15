2a0345e57f290885fa9e275775ac2b9cc0895697
Yaroslav Zvarich <zvarichjarik@gmail.com>
2024-04-15 18:19:53 +0300

    fix gifcompressor

--- utils/gifCompressor.js
+++ utils/gifCompressor.js
@@ -1,22 +1,20 @@
-// gifCompressor.js
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
 function compressGif(inputPath, outputPath, callback) {
-  // Проверяем, существует ли входной файл
   fs.access(inputPath, fs.constants.F_OK, (err) => {
     if (err) {
       callback(new Error('Input file does not exist'));
       return;
     }
 
-    // Задаем параметры сжатия
-    const optimizationLevel = '--optimize=2'; // Уровень оптимизации
-    const colorRestriction = '--colors 64'; // Ограничение на количество цветов
-    const lossyCompression = '--lossy=50'; // Использование потерь для сжатия
+    const optimizationLevel = '--optimize=2';
+    const colorRestriction = '--colors 64';
+    const lossyCompression = '--lossy=50';
+    const gifsicleExePath = path.resolve(__dirname, '..', 'gifsicle.exe');
 
-    // Запускаем процесс сжатия гиф-файла с новыми параметрами
-    exec(`gifsicle ${optimizationLevel} ${colorRestriction} ${lossyCompression} ${inputPath} -o ${outputPath}`, (error, stdout, stderr) => {
+    exec(`"${gifsicleExePath}" ${optimizationLevel} ${colorRestriction} ${lossyCompression} "${inputPath}" -o "${outputPath}"`, (error, stdout, stderr) => {
       if (error) {
         callback(error);
         return;
