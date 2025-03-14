34c63b4ad00250ec80a23c3dc5ee61a06e248f44
tapsikk <tapsgame@gmail.com>
2025-03-14 02:11:26 +0200

    fix token, and fix commands

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # myBot
 жесткий бот от тапсикааа
 
-
 node deploy-commands.js - загружает команды в дс.
 
 node index.js - запуск бота    
--- events/taps.js
+++ events/taps.js
@@ -7,11 +7,16 @@ module.exports = {
       message.reply("Привет! Я здесь!");
     }
     // Проверяем, содержит ли сообщение команду "!тапс"
-    if (message.content === 'тапс') {
+    if (message.content == 'тапс') {
       // Реагируем на команду
       message.react('👋')
         .catch(console.error);
       message.reply("я тапсик хихвыфффффихи");
+    }
+      if (message.content == 'диктор') {
+        message.react('👋')
+          .catch(console.error);
+        message.reply("чего надо от него? ААААА");
     }
   }
 }
